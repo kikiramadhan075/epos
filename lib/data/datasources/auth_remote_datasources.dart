13a349e956f465da786f14c17d10949c4033f56b
kikiramadhan075 <kikiramadhan028@gmail.com>
2024-01-13 00:43:23 +0700

    add datasources, auth login, auth response model and dashboard home

---
+++ lib/data/datasources/auth_remote_datasources.dart
@@ -0,0 +1,24 @@
+
+import 'package:epos/data/models/response/auth_response_model.dart';
+import 'package:dartz'
+import 'package:http/http.dart' as http;
+
+class AuthRemoteDatasource {
+  Future<Either<String, AuthResponseModel>> login(
+    String email,
+    String password,
+  ) async {
+    final response = await http.post(
+      Uri.parse('${Variables.baseUrl}/api/login'),
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+    if (response.statusCode == 200) {
+      return right(AuthResponseModel.fromJson(response.body));
+    } else {
+      return left(response.body);
+    }
+  }
+}
