a26e2ad2f9ecec6cf9ae60b021f71c1b1c0adfd7
kikiramadhan075 <kikiramadhan028@gmail.com>
2024-11-06 23:25:30 +0700

    edit baseUrl API

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,4 +1,4 @@
 class Variables {
-  static const String baseUrl = 'http://panel.epos.blue';
+  static const String baseUrl = 'https://prothol.epos.blue';
   static const String imageBaseUrl = '$baseUrl/storage/products/';
-}
+} 
